38b278b887b3259cb8d01ccab11b6189b329260d
Yeasaleh <yeasalehofficial2002@gmail.com>
2025-01-05 11:49:00 +0600

    what is control state ment

---
+++ Day_4_ControlFlowConditionals.js
@@ -0,0 +1,6 @@
+/* 
+
+Introduction to Control Flow:
+=> Control flow determines the order in which code is executed. In JavaScript, control flow allows us to make decisions using conditionals. This is crucial for executing specific blocks of code based on certain conditions, making programs dynamic and interactive.
+
+*/
