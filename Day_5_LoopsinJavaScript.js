d8573fadeb9a50bbc24f217529306e2c4c7aa94a
Yeasaleh <yeasalehofficial2002@gmail.com>
2025-01-05 11:58:35 +0600

    loops

--- Day_5_LoopsinJavaScript.js
+++ Day_5_LoopsinJavaScript.js
@@ -2,10 +2,19 @@
 Types of Loops:
 Loops allow repetitive tasks to be performed efficiently.
 
-for Loop:
+1. for Loop:
 The for loop is ideal when the number of iterations is known.
 for (let i = 0; i < 5; i++) {
   console.log("Iteration: " + i);
+}
+
+2. while Loop:
+The while loop runs as long as a specified condition is true.
+
+let i = 0;
+while (i < 5) {
+  console.log("Iteration: " + i);
+  i++;
 }
 
 
