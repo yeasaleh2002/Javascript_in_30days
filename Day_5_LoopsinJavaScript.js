f7f4faf3727ac4a568758ae962fd6c6c2cb8344b
Yeasaleh <yeasalehofficial2002@gmail.com>
2025-01-05 11:57:23 +0600

    loops

---
+++ Day_5_LoopsinJavaScript.js
@@ -0,0 +1,12 @@
+/* 
+Types of Loops:
+Loops allow repetitive tasks to be performed efficiently.
+
+for Loop:
+The for loop is ideal when the number of iterations is known.
+for (let i = 0; i < 5; i++) {
+  console.log("Iteration: " + i);
+}
+
+
+ */
