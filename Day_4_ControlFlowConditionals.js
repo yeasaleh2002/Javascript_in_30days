84bf4dc114448ca9c6882a110c28fcaaccad2123
Yeasaleh <yeasalehofficial2002@gmail.com>
2025-01-05 11:51:46 +0600

    what is control state ment

--- Day_4_ControlFlowConditionals.js
+++ Day_4_ControlFlowConditionals.js
@@ -3,4 +3,37 @@
 Introduction to Control Flow:
 => Control flow determines the order in which code is executed. In JavaScript, control flow allows us to make decisions using conditionals. This is crucial for executing specific blocks of code based on certain conditions, making programs dynamic and interactive.
 
+
+Conditional Statements:
+if Statement: The if statement is used to execute a block of code if a specified condition evaluates to true.
+1. 
+let age = 20;
+if (age >= 18) {
+  console.log("You are an adult.");
+}
+
+2. 
+if-else Statement:
+The if-else statement adds an alternative block of code to run if the condition is false.
+let age = 15;
+if (age >= 18) {
+  console.log("You are an adult.");
+} else {
+  console.log("You are a minor.");
+}
+
+3. if-else if-else Ladder:
+The if-else if-else structure handles multiple conditions sequentially.
+let marks = 85;
+if (marks >= 90) {
+  console.log("Grade: A+");
+} else if (marks >= 80) {
+  console.log("Grade: A");
+} else {
+  console.log("Grade: B");
+}
+
+4. Switch Statement:
+The switch statement is an alternative to multiple if-else blocks. It evaluates an expression against multiple cases.
+
 */
