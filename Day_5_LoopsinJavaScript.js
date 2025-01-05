40575be81e5358d8a80f8e885482007ea44f7171
Yeasaleh <yeasalehofficial2002@gmail.com>
2025-01-05 11:59:42 +0600

    loops

--- Day_5_LoopsinJavaScript.js
+++ Day_5_LoopsinJavaScript.js
@@ -17,5 +17,21 @@ while (i < 5) {
   i++;
 }
 
+3. do-while Loop:
+The do-while loop executes its block at least once before checking the condition.
+let i = 0;
+do {
+  console.log("Iteration: " + i);
+  i++;
+} while (i < 5);
+
+4. for-in Loop (for Objects):
+The for-in loop iterates over the keys of an object.
+
+let person = { name: "Rahim", age: 25 };
+for (let key in person) {
+  console.log(key + ": " + person[key]);
+}
+
 
  */
