518da558a15f03c41064bd5cc63d10e9719a47b7
Yeasaleh <yeasalehofficial2002@gmail.com>
2025-01-05 11:34:10 +0600

    oprator

---
+++ ay_3_OperatorsinJavaScript.js
@@ -0,0 +1,41 @@
+/* 
+Types of Operators:
+
+Arithmetic Operators:
+
++ (Addition)
+
+- (Subtraction)
+
+* (Multiplication)
+
+/ (Division)
+
+% (Modulus: Remainder)
+
+Assignment Operators:
+
+=: Assigns value (e.g., x = 10)
+
++=: Adds and assigns (e.g., x += 5)
+
+Comparison Operators:
+
+== (Equality, type-insensitive)
+
+=== (Strict equality)
+
+> (Greater than)
+
+< (Less than)
+
+!= (Not equal)
+
+Logical Operators:
+
+&& (AND)
+
+|| (OR)
+
+! (NOT)
+ */
